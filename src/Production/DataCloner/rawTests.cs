29ff99f6592afcefc9c2d192263fc0fc143930c4
Adam Paquette <nfs120@gmail.com>
2015-01-17 22:18:32 -0500

    Maj rawTests avec le nouveau script MySQL.

--- src/Production/DataCloner/rawTests.cs
+++ src/Production/DataCloner/rawTests.cs
@@ -162,57 +162,21 @@ namespace Class
 
         public static void DataclonerTest1()
         {
-            var dc = new DataCloner.Cloner();
+            var dc = new Cloner();
             RowIdentifier source = new RowIdentifier();
 
             //Map serveur source / destination
-            dc.ServerMap.Add(new ServerIdentifier { ServerId = 1, Database = "sakila", Schema = "" },
-                             new ServerIdentifier { ServerId = 1, Database = "sakila", Schema = "" });
-            dc.ServerMap.Add(new ServerIdentifier { ServerId = 1, Database = "employees", Schema = "" },
-                             new ServerIdentifier { ServerId = 1, Database = "employees", Schema = "" });
+            dc.ServerMap.Add(new ServerIdentifier { ServerId = 1, Database = "dataclonertestdatabase", Schema = "" },
+                             new ServerIdentifier { ServerId = 1, Database = "dataclonertestdatabase", Schema = "" });
 
             dc.Initialize();
 
-            /*******************
-                 Employees
-            *******************/
-            //source.ServerId = 1;
-            //source.Database = "employees";
-            //source.Schema = "";
-            //source.Table = "employees";
-            //source.Columns.Add("emp_no", 10001);
-            //dc.SqlTraveler(source, true, false);
-
-
-            /*******************
-                   SAKILA
-            *******************/
-
-            ////Basic test : 1 row
-            //source.ServerId = 1;
-            //source.Database = "sakila";
-            //source.Schema = "";
-            //source.Table = "actor";
-            //source.Columns.Add("actor_id", 1);
-            //dc.SqlTraveler(source, true, false);
-
-            ////Basic test : 1 rows with dependencies
-            //source.Columns.Clear();
-            //source.ServerId = 1;
-            //source.Database = "sakila";
-            //source.Schema = "";
-            //source.Table = "city";
-            //source.Columns.Add("city_id", 9);
-            //dc.SqlTraveler(source, true, false);
-
-            //Medium test : 1 rows with lots of dependencies
             source.Columns.Clear();
             source.ServerId = 1;
-            source.Database = "sakila";
+            source.Database = "dataclonertestdatabase";
             source.Schema = "";
-            source.Table = "customer";
-            source.Columns.Add("active", 0);
-            source.Columns.Add("address_id", 20);
+            source.Table = "customers";
+            source.Columns.Add("customerNumber", 103);
             dc.SqlTraveler(source, true, false);
         }
 
