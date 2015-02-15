978df726c251ceb5a2b291037650b47eb29e2ece
Adam Paquette <nfs120@gmail.com>
2015-02-15 18:14:39 -0500

    FakeBasicDatabase

--- src/Production/DataCloner.Tests/ClonerTests.cs
+++ src/Production/DataCloner.Tests/ClonerTests.cs
@@ -1,117 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using DataCloner.DataAccess;
+﻿using DataCloner.DataAccess;
 using DataCloner.DataClasse;
 using DataCloner.DataClasse.Cache;
 using DataCloner.DataClasse.Configuration;
-using Xunit;
+using DataCloner.Tests.Tools;
 using NSubstitute;
-using NSubstitute.Core;
+using Xunit;
 
 namespace DataCloner.Tests
 {
     public class ClonerTests
     {
-        private readonly Cache _cache;
-        private readonly Cloner _cloner;
-        private readonly IQueryHelper _queryHelper;
-        private readonly IQueryDispatcher _queryDispatcher;
-
-        public ClonerTests()
+        public class BasicClonerTests
         {
-            _cache = CreateCache();
+            private readonly Cache _cache;
+            private readonly Cloner _cloner;
+            private readonly IQueryHelper _queryHelper;
+            private readonly IQueryDispatcher _queryDispatcher;
 
-            _queryHelper = Substitute.For<IQueryHelper>();
-            _queryHelper.Select(NewRi(0, "", "", "customer", new ColumnsWithValue { { "id", 1 } }))
-                        .Returns(new[] { new object[] { 1, 2, 3 } });
-
-            _queryDispatcher = Substitute.For<IQueryDispatcher>();
-            _queryDispatcher.GetQueryHelper(0).Returns(_queryHelper);
-            _queryDispatcher.GetQueryHelper(Arg.Is<IRowIdentifier>(r => r.ServerId == 0)).Returns(_queryHelper);
-
-            _cloner = new Cloner(_queryDispatcher, (a, b, c, d, e, f) => _cache);
-            _cloner.Config = new Configuration();
-        }
-
-        public static IRowIdentifier NewRi(Int16 serverId, string database,
-            string schema, string table, ColumnsWithValue cols)
-        {
-            return new RowIdentifier
+            public BasicClonerTests()
             {
-                ServerId = serverId,
-                Database = database,
-                Schema = schema,
-                Table = table,
-                Columns = cols
-            };
-        }
+                _cache = FakeBasicDatabase.CreateCache();
+                _queryHelper = FakeBasicDatabase.CreateData();
 
+                _queryDispatcher = Substitute.For<IQueryDispatcher>();
+                _queryDispatcher.GetQueryHelper(0).Returns(_queryHelper);
+                _queryDispatcher.GetQueryHelper(Arg.Is<IRowIdentifier>(r => r.ServerId == 0)).Returns(_queryHelper);
 
-        [Fact]
-        public void QueryDispatcherCalledWithRowIdentifierFromExtensionReturnData()
-        {
-            var row = NewRi(0, "", "", "customer", new ColumnsWithValue { { "id", 1 } });
-            var result = _queryDispatcher.Select(row);
+                _cloner = new Cloner(_queryDispatcher, (a, b, c, d, e, f) => _cache);
+                _cloner.Config = new Configuration();
+            }
 
-            _queryDispatcher.Received().GetQueryHelper(row);
-            _queryHelper.Received().Select(row);
-
-            Assert.Equal(new[] { new object[] { 1, 2, 3 } }, result);
-        }
-
-        [Fact]
-        public void QueryDispatcherCalledWithRowIdentifierReturnData()
-        {
-            var row = NewRi(0, "", "", "customer", new ColumnsWithValue { { "id", 1 } });
-            var result = _queryDispatcher.GetQueryHelper(row).Select(row);
-
-            _queryDispatcher.Received().GetQueryHelper(row);
-            _queryHelper.Received().Select(row);
-
-            Assert.Equal(new[] { new object[] { 1, 2, 3 } }, result);
-        }
-
-        [Fact]
-        public void QueryDispatcherCalledWithIntegerReturnGoodData()
-        {
-            var row = NewRi(0, "", "", "customer", new ColumnsWithValue { { "id", 1 } });
-            var result = _queryDispatcher.GetQueryHelper(0).Select(row);
-
-            _queryDispatcher.Received().GetQueryHelper(row.ServerId);
-            _queryHelper.Received().Select(row);
-
-            Assert.Equal(new[] { new object[] { 1, 2, 3 } }, result);
-        }
-
-        [Fact]
-        public void BasicTest()
-        {
-            var source = NewRi(0, "", "", "customer", new ColumnsWithValue { { "id", 1 } });
-            _cloner.Clone(null, null, null, null, source, true);
-
-            _queryDispatcher.Received();
-            _queryHelper.Received();
-            _queryHelper.Received().Select(Arg.Any<IRowIdentifier>());
-            _queryHelper.Received().Select(NewRi(0, "", "", "customer", new ColumnsWithValue { { "id", 1 } }));
-        }
-
-        private static Cache CreateCache()
-        {
-            var customerTable = new TableSchema();
-            customerTable.Name = "customer";
-            customerTable.ColumnsDefinition = new[]
+            [Fact]
+            public void QueryDispatcherCalledWithRowIdentifierFromExtensionReturnData()
             {
-                new ColumnDefinition {Name = "pk", IsPrimary = true, Type = DbType.Int32},
-                new ColumnDefinition {Name = "name", Type = DbType.String, Size = "20"}
-            };
+                var row = NewRi(0, "", "", "customer", new ColumnsWithValue {{"id", 1}});
+                var result = _queryDispatcher.Select(row);
 
-            var cache = new Cache();
-            cache.ServerMap.Add(new ServerIdentifier { Database = "", Schema = "" },
-                                new ServerIdentifier { Database = "", Schema = "" });
-            cache.DatabasesSchema.Add(0, "", "", customerTable);
-            return cache;
+                _queryDispatcher.Received().GetQueryHelper(row);
+                _queryHelper.Received().Select(row);
+
+                Assert.Equal(new[] {new object[] {1, 2, 3}}, result);
+            }
+
+            [Fact]
+            public void QueryDispatcherCalledWithRowIdentifierReturnData()
+            {
+                var row = NewRi(0, "", "", "customer", new ColumnsWithValue {{"id", 1}});
+                var result = _queryDispatcher.GetQueryHelper(row).Select(row);
+
+                _queryDispatcher.Received().GetQueryHelper(row);
+                _queryHelper.Received().Select(row);
+
+                Assert.Equal(new[] {new object[] {1, 2, 3}}, result);
+            }
+
+            [Fact]
+            public void QueryDispatcherCalledWithIntegerReturnData()
+            {
+                var row = NewRi(0, "", "", "customer", new ColumnsWithValue {{"id", 1}});
+                var result = _queryDispatcher.GetQueryHelper(0).Select(row);
+
+                _queryDispatcher.Received().GetQueryHelper(row.ServerId);
+                _queryHelper.Received().Select(row);
+
+                Assert.Equal(new[] {new object[] {1, 2, 3}}, result);
+            }
+
+            [Fact]
+            public void BasicTest()
+            {
+                var source = NewRi(0, "", "", "customer", new ColumnsWithValue {{"id", 1}});
+                _cloner.Clone(null, null, null, null, source, true);
+
+                _queryDispatcher.Received();
+                _queryHelper.Received();
+                _queryHelper.Received().Select(Arg.Any<IRowIdentifier>());
+                _queryHelper.Received().Select(NewRi(0, "", "", "customer", new ColumnsWithValue {{"id", 1}}));
+            }
         }
     }
 }
---
+++ src/Production/DataCloner.Tests/FakeBasicDatabase.cs
@@ -0,0 +1,88 @@
+using System.Data;
+using DataCloner.DataAccess;
+using DataCloner.DataClasse;
+using DataCloner.DataClasse.Cache;
+using NSubstitute;
+
+namespace DataCloner.Tests
+{
+    internal class FakeBasicDatabase
+    {
+        internal static Cache CreateCache()
+        {
+            var city = new TableSchema
+            {
+                Name = "city",
+                ColumnsDefinition = new[]
+                {
+                    new ColumnDefinition {Name = "id", IsPrimary = true, Type = DbType.Int32},
+                    new ColumnDefinition {Name = "name", Type = DbType.String, Size = "20"}
+                }
+            };
+
+            var state = new TableSchema
+            {
+                Name = "state",
+                ColumnsDefinition = new[]
+                {
+                    new ColumnDefinition {Name = "id", IsPrimary = true, Type = DbType.Int32},
+                    new ColumnDefinition {Name = "name", Type = DbType.String, Size = "20"},
+                    new ColumnDefinition {Name = "cityId", IsForeignKey = true, Type = DbType.Int32}
+                },
+                ForeignKeys = new[]
+                {
+                    new ForeignKey
+                    {
+                        ServerIdTo = 0,
+                        DatabaseTo = "",
+                        SchemaTo = "",
+                        TableTo = "city",
+                        Columns = new []{new ForeignKeyColumn {NameFrom = "cityId", NameTo = "id"} }
+                    }
+                },
+                DerivativeTables = new[] { new DerivativeTable { ServerId = 0, Schema = "", Database = "", Table = "city" } }
+            };
+
+            var country = new TableSchema
+            {
+                Name = "country",
+                ColumnsDefinition = new[]
+                {
+                    new ColumnDefinition {Name = "id", IsPrimary = true, Type = DbType.Int32},
+                    new ColumnDefinition {Name = "name", Type = DbType.String, Size = "20"},
+                    new ColumnDefinition {Name = "stateId", IsForeignKey = true, Type = DbType.Int32}
+                },
+                ForeignKeys = new[]
+                {
+                    new ForeignKey
+                    {
+                        ServerIdTo = 0,
+                        DatabaseTo = "",
+                        SchemaTo = "",
+                        TableTo = "state",
+                        Columns = new []{new ForeignKeyColumn {NameFrom = "stateId", NameTo = "id"} }
+                    }
+                },
+                DerivativeTables = new[] { new DerivativeTable { ServerId = 0, Schema = "", Database = "", Table = "contry" } }
+            };
+
+            var cache = new Cache();
+            cache.ServerMap.Add(new ServerIdentifier { Database = "", Schema = "" },
+                new ServerIdentifier { Database = "", Schema = "" });
+            cache.DatabasesSchema.Add(0, "", "", city);
+            cache.DatabasesSchema.Add(0, "", "", state);
+            cache.DatabasesSchema.Add(0, "", "", country);
+
+            return cache;
+        }
+
+        internal static IQueryHelper CreateData()
+        {
+            var queryHelper = Substitute.For<IQueryHelper>();
+            queryHelper.Select(Tools.NewRi(0, "", "", "customer", new ColumnsWithValue { { "id", 1 } }))
+                .Returns(new[] { new object[] { 1, 2, 3 } });
+
+            return queryHelper;
+        }
+    }
+}
