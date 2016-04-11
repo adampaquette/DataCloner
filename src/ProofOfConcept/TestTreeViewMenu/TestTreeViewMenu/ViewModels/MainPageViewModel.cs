084e2e15fc4f1d3e71c2f5afea37610760513a36
Adam Paquette <nfs120@gmail.com>
2016-04-10 23:01:35 -0400

    Preuve de concept TreeView

---
+++ src/ProofOfConcept/TestTreeViewMenu/TestTreeViewMenu/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TestTreeViewMenu.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<MenuItem> MenuItems { get; set; }
+
+        public MainPageViewModel()
+        {
+            MenuItems = new ObservableCollection<MenuItem>()
+            {
+                new MenuItem()
+                {
+                    Text = "MY ACCOUNT",
+                    Children = new ObservableCollection<MenuItem>
+                    {
+                        new MenuItem{ Text = "Overview============================================" },
+                        new MenuItem{ Text = "History" },
+                        new MenuItem{ Text = "Profile" }
+                    }
+                },
+                new MenuItem()
+                {
+                    Text = "SOLUTION EXPLORER",
+                    Children = new ObservableCollection<MenuItem>
+                    {
+                        new MenuItem
+                        {
+                            Text = "Project",
+                            Children = new ObservableCollection<MenuItem>
+                            {
+                                new MenuItem { Text = "ViewModels", Children = new ObservableCollection<MenuItem>() { new MenuItem { Text = "MainPageViewModel.cs" } } },
+                                new MenuItem { Text = "App.xaml", Children = new ObservableCollection<MenuItem>() { new MenuItem { Text = "App.xaml.cs" } } }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+    }
+}
