75baff9987a4de96a1e36f7b141ea6696a7f1204
Adam Paquette <nfs120@gmail.com>
2016-08-26 21:50:10 -0400

    New folders : ui.photoshop, ui.prototype

---
+++ prototype/TestTreeViewMenu/ViewModels/MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TestTreeViewMenu.ViewModels
+{
+    public class MenuItem : ViewModelBase
+    {
+        private string _text;
+        private ObservableCollection<MenuItem> _children;
+
+        public string Text
+        {
+            get { return _text; }
+            set { Set(ref _text, value); }
+        }
+
+        public ObservableCollection<MenuItem> Children
+        {
+            get { return _children; }
+            set { Set(ref _children, value); }
+        } 
+    }
+}
